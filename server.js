d27862bb35a20d66588710dbe495f4251d9e5827
Austen Haymond <ahaymon17@gmail.com>
2020-02-06 20:17:47 -0700

    created reservation contructor

--- server.js
+++ server.js
@@ -9,8 +9,34 @@ app.use(express.json());
 
 console.log("epic! this works!");
 
+class Reservation {
+    constructor(name, phone, email, id) {
+      this.name = name;
+      this.phone = phone;
+      this.email = email;
+      this.id = id;
+      this.table = "";
+    }
+    getName() {
+      console.log(`Hello, my name is ${this.name}`);
+      return this.name;
+    }
+    getId() {
+      console.log(`  ${this.id}`);
+      return this.id;
+    }
+    getEmail() {
+      return this.email;
+    }
+    getRole() {
+      return this.role;
+    }
+  };
+  module.exports = Reservation;
 
-
+app.get("/", function(req, res) {
+    req.sendFile(path.join(__dirname, ""))
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
